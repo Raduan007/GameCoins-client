faf48316f60ea1f30e898166a7c4e58e6f18943f
raduan007 <raduanhossen517@gmail.com>
2026-07-12 18:35:43 +0600

    feat(client): connect frontend with backend api

--- src/models/game.model.ts
+++
@@ -1,8 +0,0 @@
-import mongoose, { Model } from "mongoose";
-import { GameSchema } from "./game.schema";
-import type { IGame } from "@/types/game";
-
-const Game: Model<IGame> =
-  mongoose.models.Game || mongoose.model<IGame>("Game", GameSchema);
-
-export default Game;
